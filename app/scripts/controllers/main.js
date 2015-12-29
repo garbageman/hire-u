e0c4371af2d2cb435edb766f128d3f9d25129474
rybredd <rzimpo@umd.edu>
2015-12-29 18:30:38 -0500

    fleshed out some job placeholders + angular controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,19 @@
  * Controller of the hireUApp
  */
 angular.module('hireUApp')
-  .controller('MainCtrl', function () {
-    
-  });
+.controller('MainCtrl', ['$scope', function($scope) {
+	$scope.jobs = [
+		{
+			"title": "Job 1",
+			"text": "text for Job1"
+			
+		}, {
+			"title": "Job 2",
+			"text": "text for Job2"
+			
+		}, {
+			"title": "Job 3",
+			"text": "text for Job3"
+		}
+	];
+}]);
